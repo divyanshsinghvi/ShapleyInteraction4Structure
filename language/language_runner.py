f8de60a8c9ee05aa363159702a35afec824a55e0
Divyansh Singhvi <divyanshsinghvi@gmail.com>
2025-09-15 01:20:00 +0530

    correct version update

--- language/language_runner.py
+++ language/language_runner.py
@@ -1,4 +1,3 @@
-
 from transformers import BertForMaskedLM, BertTokenizerFast
 from transformers import GPT2LMHeadModel, GPT2TokenizerFast
 import pandas as pd
@@ -7,6 +6,10 @@ from tqdm import tqdm
 import numpy as np
 import pickle
 import random
+import gzip
+import os
+import gc
+
 
 class ExperimentRunner:
     def __init__(self, cuda, seq_len, model_name, method):
@@ -16,8 +19,54 @@ class ExperimentRunner:
         self.MODEL_NAME = model_name
         self.METHOD = method
         self.SOFTMAX = False
+        self.RIGHT_MAX = 6
+        self.NUMBER_OF_PERM = 4
+        self.NUMBER_OF_ROWS = 4
         random.seed(42)
+
+    ## NEED TO MOVE IT TO SOME HELPER CLASS    
+    def generate_perm(self, nperm):
+        perm_stored=[]
+        arg = np.arange(self.SEQ_LEN)
+        i = 0
+        while i < nperm: 
+            perm_stored.append(np.random.permutation(arg))
+            perm_stored.append(perm_stored[-1][::-1])
+            i += 1
+        return np.array(perm_stored)
+
+    def generate_incremental_masks(self, permutations, l, r):
+        n_rows, n_cols = permutations.shape
+        
+        masks = np.zeros((n_rows, n_cols), dtype=np.int8)
+        
+        stop_mask = (permutations == l) | (permutations == r)
+        stop_indices = np.argmax(stop_mask, axis=1)
+        col_indices = np.arange(n_cols)[np.newaxis, :]
+        valid_mask = col_indices < stop_indices[:, np.newaxis]
+        indices = permutations * valid_mask
+
+        
+        for inde, row in enumerate(indices):
+            non_zero_indices = row[row != 0]
+            masks[inde, non_zero_indices] = True
+        return masks
     
+    def get_smax(self, encoded_row, masks_tensor):
+        expanded_input = encoded_row.unsqueeze(0).expand(masks_tensor.size(0), -1, -1)
+        masks_tensor_expanded = masks_tensor.unsqueeze(1).expand(-1, encoded_row.shape[0], -1)
+        masked_tensors = torch.where(masks_tensor_expanded == 1, expanded_input, torch.tensor(self.tokenizer.pad_token_id, device='cuda:0'))
+
+        masked_tensors = masked_tensors.flatten(start_dim = 0, end_dim = 1)
+        # masked_tensors = masked_tensors.reshape((masked_tensors.shape[0] * masked_tensors.shape[1], -1))
+
+        
+        masks_tensor_expanded = masks_tensor_expanded.flatten(start_dim = 0, end_dim = 1)
+        # masks_tensor_expanded = masks_tensor_expanded.reshape((masks_tensor_expanded.shape[0] * masks_tensor_expanded.shape[1], -1))
+        logits = self.model(masked_tensors, attention_mask=masks_tensor_expanded).logits    
+        smax = logits.softmax(dim=-1)
+        return smax, logits
+
     def prepare_model(self):
         if self.MODEL_NAME == 'bert':
             if self.CUDA:
@@ -25,7 +74,7 @@ class ExperimentRunner:
             else:
                 self.model = BertForMaskedLM.from_pretrained("bert-base-uncased")
 
-            self.tokenizer= BertTokenizerFast.from_pretrained('bert-base-uncased', use_fast=True)#, add_special_tokens=False)
+            self.tokenizer= BertTokenizerFast.from_pretrained('bert-base-uncased', use_fast=True, add_special_tokens=False)
 
         elif self.MODEL_NAME == 'gpt':
             if self.CUDA:
@@ -52,140 +101,110 @@ class ExperimentRunner:
         test['length'] = test['sentence'].str.split().str.len()
         test = test[~((test['weak_mwe'].str.len()==0) & (test['strong_mwe'].str.len()==0))]
         test = test.drop(columns = ['syntactic_distance_idx', 'lemmas', 'd', 'toks', 'tags'], errors='ignore')
+        test = test.rename_axis('row_number').reset_index()
+        print(len(test))
         self.test = test
 
 
-    def get_prediction_softmax(self, X, token_next):
-        logits = self.model(X).logits
-        if self.SOFTMAX:
-            abc =  logits[0, token_next:, :].softmax(dim=-1)
-        else:
-            abc =  logits[0, token_next:, :]
+    def interaction_value_di(self, X, start_end_row):
+        perms = self.generate_perm(self.NUMBER_OF_PERM)
+        results = []
 
-        if not self.CUDA:
-            return abc.detach().numpy()
-        else:
-            return abc
+
+        for l in range(self.SEQ_LEN):
+            for r in range(l+1, min(self.SEQ_LEN, l+self.RIGHT_MAX + 1)):
+                masks = self.generate_incremental_masks(perms, l, r)
+                masks_tensor = torch.tensor(masks, dtype=torch.long, device='cuda:0').detach()
+
+                smax_ab, logits_ab = self.get_smax(X, masks_tensor.clone())
+
+                masks_tensor_A = masks_tensor.clone()
+                masks_tensor_A[:, l] = 1
+                smax_a, logits_a = self.get_smax(X, masks_tensor_A)
+
+                masks_tensor_B = masks_tensor.clone()
+                masks_tensor_B[:, r] = 1
+                smax_b, logits_b = self.get_smax(X, masks_tensor_B)
+
+                masks_tensor_phi = masks_tensor.clone()
+                masks_tensor_phi[:, r] = 1
+                masks_tensor_phi[:, l] = 1
+                smax_phi, logits_phi = self.get_smax(X, masks_tensor_phi)
+
+
+                smax = smax_ab - smax_a - smax_b + smax_phi
+                logits = logits_ab - logits_a - logits_b + logits_phi
+
+
+                smax = smax.unflatten(0, (smax.shape[0]//X.shape[0], X.shape[0]))
+                logits = logits.unflatten(0, (logits.shape[0]//X.shape[0], X.shape[0]))
+
+                smax = torch.linalg.norm(smax, dim=-1)
+
+                logits = torch.linalg.norm(logits, dim=-1)
+                smax = torch.mean(smax, dim=0).cpu().detach()
+                logits = torch.mean(logits, dim=0).cpu().detach()
+
+                smax_ab = torch.linalg.norm(smax_ab, dim=-1).cpu().detach()
+                smax_ab = smax_ab.unflatten(0, (smax_ab.shape[0]//X.shape[0], X.shape[0]))
+                smax_ab = torch.mean(smax_ab, dim=0).cpu().detach()
+
+                results.append([l, r, smax, logits, smax_ab, start_end_row])
+        return results
+
     
-    def interaction_value_di(self, X, tokens):
-        token1, token2 = tokens
-        if self.MODEL_NAME == 'gpt':
-            token_next = max(token1, token2) + 1
-        elif self.MODEL_NAME == 'bert':
-            token_next = 0
-
-        AB = self.get_prediction_softmax(X, token_next)
-        X_t1 = X.clone()
-        X_t1[0, token1] = self.tokenizer.pad_token_id
-        A = self.get_prediction_softmax(X_t1, token_next)
-        
-        X_t2 = X.clone()
-        X_t2[0,token2] = self.tokenizer.pad_token_id
-        B = self.get_prediction_softmax(X_t2, token_next)
-
-        X_t12 = X.clone()
-        X_t12[0,token2] = self.tokenizer.pad_token_id
-        X_t12[0,token1] = self.tokenizer.pad_token_id
-        phi = self.get_prediction_softmax(X_t12, token_next)
-
-        # print(AB, A, B, phi)
-        val = AB - A - B + phi
-        
-
-        if self.METHOD == 1:
-            val = AB - A - B + phi
-            val = torch.divide(torch.linalg.norm(val, dim=1), torch.linalg.norm(AB, dim=1)).cpu()
-            res_list = [(1, val.detach(), token_next)]
-            return res_list
-
-
-    
-    def mwe_distance_interaction(self, encoded_row, row, col, row_number):
-
-        iv_mwe = []
-        
-        mwes = row[col]
-        encoded_row = encoded_row.reshape(1, -1)
-        for mwe in mwes:
-
-            for i in range(len(mwe)):
-                for j in range(len(mwe)):
-                    if i > j:
-                        if len([x for x in mwe if x >= self.SEQ_LEN]) > 0:
-                            continue
-                        iv = self.interaction_value_di(encoded_row, [mwe[i]-1, mwe[j]-1])
-                        iv_mwe.append([iv, abs((mwe[i]-1-(mwe[j]-1))),mwe, row_number, i, j])
-                        
-        return iv_mwe
-    
-    def calculate_interaction(self, encoded_row, row_number):
+    def calculate_interaction(self, encoded_row, start_end_row):
         interactions = []
-        encoded_len = len(encoded_row)
-        for j in range( min(self.SEQ_LEN, encoded_len)):
-            probability = 0.05
-            if random.random() < probability: 
-                for k in range(j+1, min(self.SEQ_LEN, encoded_len, j+9)):
-                    if j+k >= encoded_len:
-                        continue
-                    if encoded_row[j] == self.tokenizer.unk_token_id or encoded_row[k] == self.tokenizer.unk_token_id:
-                        continue
-
-                    og = encoded_row.clone()
-                    og = og.reshape(1, -1)
-                    iv = self.interaction_value_di(og, [j, k])
-                    interactions.append([iv, abs((j-k)), row_number, j, k])
-                
+        iv = self.interaction_value_di(encoded_row, start_end_row)
+        interactions.append(iv)
         return interactions
     
-    def run_mwe_interactions(self, suffix=''):
-        weak_mwe_distance = []
-        strong_mwe_distance = []
-
-
-        for row_number, row in tqdm(self.test.iterrows(), total=self.test.shape[0]):
-            # Tokenize each sentence with no padding and truncate if seq_len < tokenized sentence length
-            encoded_row = self.tokenizer(row['sentence'], padding=False,  truncation=True, max_length=self.SEQ_LEN, return_tensors ='pt').input_ids[0]
-            if self.CUDA:
-                encoded_row = encoded_row.cuda()
-
-            if len(row['weak_mwe']) != 0:
-                weak_mwe_distance.append(self.mwe_distance_interaction(encoded_row, row, 'weak_mwe', row_number))
-            
-            if len(row['strong_mwe']) != 0:
-                strong_mwe_distance.append(self.mwe_distance_interaction(encoded_row, row, 'strong_mwe', row_number))
-            
-            del encoded_row
-        
-        weak_mwe_distance = [y for x in weak_mwe_distance for y in x]
-        strong_mwe_distance = [y for x in strong_mwe_distance for y in x]
-
-        pd.DataFrame(weak_mwe_distance, columns = ['tensor', 'posdis', 'ignore', 'row_number', 'first_token', 'second_token']).to_pickle(f'weak_mwe_{self.MODEL_NAME}{suffix}.pkl', compression='gzip')
-        pd.DataFrame(strong_mwe_distance, columns = ['tensor', 'posdis', 'ignore', 'row_number', 'first_token', 'second_token']).to_pickle(f'strong_mwe_{self.MODEL_NAME}{suffix}.pkl', compression='gzip')
 
     def run_avg_interactions(self, suffix=''):
         average_distance = []
 
-        for row_number, row in tqdm(self.test.iterrows(), total=self.test.shape[0]):
-            encoded_row =  self.tokenizer(row['sentence'], padding=False,  truncation=True, max_length=self.SEQ_LEN, return_tensors ='pt').input_ids[0]
+        i = 1
+        for start_row in tqdm(range(0, self.test.shape[0], self.NUMBER_OF_ROWS), total=self.test.shape[0] // self.NUMBER_OF_ROWS + 1):
+
+        # for row_number, row in tqdm(self.test.iterrows(), total=self.test.shape[0]):
+            end_row = min(start_row +  self.NUMBER_OF_ROWS-1, self.test.shape[0])
+            if os.path.exists(f'output2/avg_2_{self.MODEL_NAME}{suffix}_{1000*i}.pkl'):
+                if end_row <= 1000*i:
+                    continue
+                else:
+                    print(f"{i} {1000*i}")
+                    i=i+1
+
+            batch = self.test.iloc[start_row:end_row+1]['sentence'].to_list()
+            encoded_row =  self.tokenizer(batch, padding='max_length',  truncation=True, max_length=self.SEQ_LEN, return_tensors ='pt').input_ids.detach()
             if self.CUDA:
                 encoded_row = encoded_row.cuda()
 
-            
-            if len(row['weak_mwe']) != 0:
-                average_distance.extend(self.calculate_interaction(encoded_row, row_number))
-            if row_number % 1000 == 0:
+            try:
+                average_distance.extend(self.calculate_interaction(encoded_row, list(range(start_row, end_row+1))))
+            except:
+                print("Pass")
+                pass
+            if (end_row) >= 1000*i:
+                print(f"{end_row+1} {1000*i}")
+                
                 print(len(average_distance))
-                pickle.dump(average_distance, open(f'avg_{self.MODEL_NAME}{suffix}.pkl','wb'))
+                with gzip.open(f'output2/avg_2_{self.MODEL_NAME}{suffix}_{1000*i}.pkl','wb') as f:
+                    pickle.dump(average_distance, f)
+                i += 1
+                del average_distance[:]
+                gc.collect()
 
-    def run_experiment(self, mwe=True, avg=True, suffix=''):
+        with gzip.open(f'output2/avg_2_{self.MODEL_NAME}{suffix}_all.pkl','wb') as f:
+            pickle.dump(average_distance, f)
+
+
+    def run_experiment(self, avg=True, suffix=''):
         self.prepare_data()
         self.prepare_model()
-        if mwe:
-            self.run_mwe_interactions(suffix=suffix)
         if avg:
             self.run_avg_interactions(suffix=suffix)
 
 if __name__  == '__main__':
-    ExperimentRunner(cuda=True, seq_len=50, model_name = 'bert', method=1).run_experiment(suffix='') 
-    ExperimentRunner(cuda=True, seq_len=50, model_name = 'gpt', method=1).run_experiment(suffix='')
-
+    ExperimentRunner(cuda=True, seq_len=20, model_name = 'gpt', method=1).run_experiment(suffix='')
+    ExperimentRunner(cuda=True, seq_len=20, model_name = 'bert', method=1).run_experiment(suffix='') 
